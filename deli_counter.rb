3dfa787377bf11a83692afa846d81d08aa7dcc53
Huy Do <huyddo40@gmail.com>
2019-05-22 14:30:13 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,14 +1,14 @@
 def line(arr)
   if !arr.empty?
-		print "The line is currently:"
+		print "The line is currently: "
 		arr.each_with_index do |name, index|
 		  # print " ",index+1,". ",name
-		  str = 
+		  str += "#{index+1}. #{name} "
 		end
 	else
 		puts "The line is currently empty."
 	end
-
+  str
 end
 
 def take_a_number(arr,name)
